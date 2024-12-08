bba465a21785a61a971485ad2e6730f90f33f568
Konrad Zagozda <kzagozda@splunk.com>
2024-12-08 19:17:55 +0100

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -124,6 +124,7 @@ export function App(): ReactElement {
             } as TimelineItem & { rawDate: Date };
           })
           .sort((a, b) => b.rawDate.getTime() - a.rawDate.getTime())
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           .map(({ rawDate, ...item }) => item);
 
         setItems(transformedItems);
