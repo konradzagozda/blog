df82e4cef874050dd5c3e001cc088149c860fc2b
Konrad Zagozda <kzagozda@splunk.com>
2024-12-09 21:17:51 +0100

    tflint

--- tf/terraform.tfvars
+++ tf/terraform.tfvars
@@ -1,4 +1,3 @@
-aws_region             = "us-east-1"
 website_bucket_name    = "konradzagozda-website-bucket-20240319"
 domain_name            = "zagozdakonrad.com"
 alternate_domain_names = ["konradzagozda.com"]
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "website_bucket_name" {
   description = "Name of the S3 bucket for the website"
   type        = string
